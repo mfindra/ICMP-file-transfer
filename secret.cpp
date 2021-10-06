6ea58f20203bfcb81a2ba9b624d5452ac018266b
mfindra <miskofindra@gmail.com>
2021-10-06 10:26:40 +0200

    parsing CL argumnets

--- secret.cpp
+++ secret.cpp
@@ -1,5 +1,40 @@
-#include <stdio.h>
+#include <unistd.h>
 
-int main(int argc, char *argv[]) {
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char** argv) {
+    int DEBUG = 1;
+
+    string R_opt;
+    string S_opt;
+    bool L_opt = false;
+
+    int opt;
+    while ((opt = getopt(argc, argv, "r:s:l")) != -1) {
+        switch (opt) {
+            case 'r':
+                R_opt = optarg;
+                break;
+            case 's':
+                S_opt = optarg;
+                break;
+            case 'l':
+                L_opt = true;
+                break;
+            default:
+                fprintf(stderr, "ERROR - Wrong argument!\n");
+                return 1;
+        }
+    }
+
+    if (DEBUG) {
+        cout << "R_opt = " << R_opt << endl;
+        cout << "S_opt = " << S_opt << endl;
+        cout << "L_opt = " << L_opt << endl;
+    }
+
     return 0;
 }
